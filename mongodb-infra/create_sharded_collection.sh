43a0cf7923366d5eed86c6e09a5401089ff12a64
vegura <annadominie@gmail.com>
2021-12-04 23:07:49 +0200

    Remove old MongoDB structure files.

--- mongodb-infra/create_sharded_collection.sh
+++
@@ -1,8 +0,0 @@
-docker exec -it mongors1n1 bash -c "echo 'use qqqq' | mongo"
-sleep 10
-docker exec -it mongos1 bash -c "echo 'sh.enableSharding(\"qqqq\")' | mongosh "
-sleep 10
-docker exec -it mongors1n1 bash -c "echo 'db.createCollection(\"qqqq.aaaa\")' | mongosh "
-sleep 10
-docker exec -it mongos1 bash -c "echo 'sh.shardCollection(\"qqqq.aaaa\", {\"name\" : 1})' | mongosh "
-sleep 10
